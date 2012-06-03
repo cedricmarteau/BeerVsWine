c2d2150df2dfcc7bbad0c8ff3ec88d6d4f0f98f1
ghumbert <humbertgregoire@free.fr>
2012-06-03 17:23:52 +0200

    prodmondial5

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 //#production_mondial
 
 $(document).ready(function(){
-	$('#vin_2 img').height(0).animate({'height':'+=106px'}, 500, 'swing');
+
+
+	$('#vin_2 img').height(0).animate({'height':'+=106px'}, 300, 'linear').function(){$('#vin_2 img').height(0).animate({'height':'+=106px'}, 300, 'linear');};
+	//$('#biere_2 img').height(0).animate({'height':'+=605px'}, 500, 'linear');
+
+
 });
 
 
