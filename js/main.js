04f767dcf060c245b4bb88c5094e44063068b284
Tim <timothee.roussilhe@gmail.com>
2012-06-03 21:36:32 +0200

    Navigation aux flèches complète.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var animating=false;
 $(document).ready(function(){
 	resizeContent();
 	setZindex();
+	setSubZindex();
 });
 
 
@@ -37,18 +38,38 @@ function setZindex(){
 		}
 	});
 }
+
+function setSubZindex(){
+	var subPages = $("#content").find('.subpages');
+	subPages.each(function(index){
+		if($(this).hasClass("subCurrent")){
+			$(this).css('z-index',"30");
+		}else{
+			$(this).css("z-index",subPages.length-index)
+		}
+	});
+}
+
 function setPagePos(){
 	var pages = $("#content").find('.pages');
 	pages.each(function(index){
-		pages.css("top","0");
+		$(this).css("top","0");
+	});
+}
+function setSubPagePos(){
+	var subPages = $("#content").find('.subpages');
+	subPages.each(function(index){
+		$(this).css("left","0");
 	});
 }
 
 $(document).keydown(function(event){
-	var pages = $("#content").find('.pages');
-	var current = $("#content").find('.current');
-	var indexCurrent=current.index();
-	if(event.which=="40"){
+	console.log(event.which);
+		if(event.which=="40"){
+			var pages = $("#content").find('.pages');
+			var current = $("#content").find('.current');
+			var indexCurrent=current.index();
+			
 		if(pages.length>indexCurrent+1 && !animating){
 			animating=true;
 			pages.eq(indexCurrent+1).css('z-index',"29");
@@ -63,6 +84,10 @@ $(document).keydown(function(event){
 		}
 	}
 	if(event.which=="38"){
+		var pages = $("#content").find('.pages');
+		var current = $("#content").find('.current');
+		var indexCurrent=current.index();
+		
 		if(indexCurrent>0 && !animating){
 			animating=true;
 			pages.eq(indexCurrent-1).css('z-index',"29");
@@ -76,5 +101,47 @@ $(document).keydown(function(event){
 			
 		}
 	}
+	if(event.which=="39"){
+		var current = $("#content").find('.current');
+		var pages = $("#content").find('.pages');
+		var subCurrent = current.find(".subCurrent");
+		var indexSubCurrent = subCurrent.index();
+		var subPages = current.find(".subpages");
+		
+		if(subPages.length>indexSubCurrent+1 && !animating){
+			animating=true;
+			subPages.eq(indexSubCurrent+1).css("z-index","29");
+			
+			current.find(".subCurrent").animate({"left":-windowWidth}, 1000,"easeInQuint",function(){
+				animating=false;
+				subCurrent.removeClass("subCurrent");
+				subPages.eq(indexSubCurrent+1).addClass("subCurrent");
+				setSubZindex();
+				setSubPagePos();
+			});
+		}
+	}
+	if(event.which=="37"){
+		var current = $("#content").find('.current');
+		var pages = $("#content").find('.pages');
+		var subCurrent = current.find(".subCurrent");
+		var indexSubCurrent = subCurrent.index();
+		var subPages = current.find(".subpages");
+		
+		if(indexSubCurrent>0 && !animating){
+			animating=true;
+			subPages.eq(indexSubCurrent-1).css("z-index","29");
+			
+			current.find(".subCurrent").animate({"left":+windowWidth}, 1000,"easeInQuint",function(){
+			animating=false;
+			subCurrent.removeClass("subCurrent");
+			subPages.eq(indexSubCurrent-1).addClass("subCurrent");
+			
+			setSubZindex();
+			setSubPagePos();
+			});
+		}
+	}
+	
 	
 });
