125871bac8f6d015814df3872bacf1c016204d6e
Tim <timothee.roussilhe@gmail.com>
2012-06-03 11:45:58 +0200

    1.1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-$(document).ready(function{
-	resizecontent();
-
+$(document).ready(function(){
+	resizeContent();
 });
 
 // Fire on window resize.
@@ -8,44 +7,15 @@ $(window).resize(function() {
 	resizeContent();
 });
 
+
 function resizeContent(){
 	////console.log("resize");
 	// Resize content to fit window width.
-	windowWidth = $(window).width();
+	var windowWidth = $(window).width();
 				
-	windowHeight = $(window).height();
-//		var realisation = $('.realisation');
-//		var sidebarWidth = $('#sidebar').width();
-//		var contentWidth = windowWidth - sidebarWidth;
-//		var lengthW = realisation.length;
-//		var totalWidth = contentWidth*(lengthW);
-//		
-//		$("#content").css('height',windowHeight);
-//		$('.pages').css('width',contentWidth);
-//		$('.about-exp').css('width',contentWidth);
-//		$('.about-section').css('width',(contentWidth-4)/3);
-//		
-//		var workWidth = (windowWidth)-(sidebarWidth+3);
-//		
-//		if (openProject == true){
-//			var id = $('.realisation.open').index();
-//			var realPosition = (((-contentWidth)*id));
-//			var newLeftPos = (-workWidth-1)*(id);
-//			
-//			realisation.css('width',workWidth);
-//			$('.work-list').css('left',newLeftPos).css('width',totalWidth-1);
-//		};
-//		
-		////console.log('width : ',contentWidth,'newPos : ',realPosition);
-//		
-//		$("#content section.pages").each(function(index,item){
-			////console.log($(this).attr("id"));
-//			if ($(this).attr("id")!=currentPage){
-//				$(this).css("top",windowHeight);
-//				$(this).css("display","none");
-//			}
-//		});
-//		
-//		scrolling();	
-//		resizeImageBackground();
+	var windowHeight = $(window).height();
+	
+	var content = $("#content");
+	content.css({'width' : windowWidth, 'height' : windowHeight}).find(".pages").css({'width' : windowWidth, 'height' : windowHeight});
+	
 }
