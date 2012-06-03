b073176348ad4a86d9f12237054a080acb26f2f7
cedricmarteau <cedric.marteau@gmail.com>
2012-06-03 19:26:14 +0200

    animation

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@
 $(document).ready(function(){
 
 
-	$('#vin_2 img').height(0).animate({'height':'+=106px'}, 300, 'linear').function(){$('#vin_2 img').height(0).animate({'height':'+=106px'}, 300, 'linear');};
+	//$('#vin_2 img').height(0).animate({'height':'+=106px'}, 300, 'linear').function(){$('#vin_2 img').height(0).animate({'height':'+=106px'}, 300, 'linear');};
 	//$('#biere_2 img').height(0).animate({'height':'+=605px'}, 500, 'linear');
-
-
+	data_production();
+	import_export();
 });
 
 
@@ -15,12 +15,27 @@ $(document).ready(function(){
 function data_production(){
 	var data_production_wine= new Array(50.50,47.12,36.45,23.00,15.50,14.50,10.50,9.62,8.33,8.28);
 	for (var i=0;i<10;i++){
-		$("#vin_import li.diagram").eq(i).animate({
+		$("#production_vin li.diagram").eq(i).animate({
 			width: data_production_wine[i]*2+"px"
-		}, 500, 'swing');
+		}, 500, 'easeInOutQuint');
 	}
 }
-
+function import_export(){
+	var data_import_wine= new Array(37,6.8,6.6,6.3,5.6,5.2,2.5,2.4,2.2,1.1);
+	var data_export_wine= new Array(34.9,18.0,9.3,8.7,4.3,3.6,3.5,3.0,2.4,1.8);
+	var data_export_beer= new Array(21,19,13,5,4.9,4,3.4,2.8,2.5,2.1);
+	for (var i=0;i<10;i++){
+		$("#biere_import li.diagram").eq(i).animate({
+			width: data_import_wine[i]*6+"px"
+		}, 1500, 'linear');
+		$("#biere_export li.diagram").eq(i).animate({
+			width: data_export_wine[i]*6+"px"
+		}, 1500, 'linear');
+		$("#vin_export li.diagram").eq(i).animate({
+			width: data_export_beer[i]*6+"px"
+		}, 1500, 'linear');
+	}
+}
 
 //#production_pays
 
