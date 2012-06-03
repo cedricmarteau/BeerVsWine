740ea3c3cab6802b1c09aaa6993fd7bf6ded5f08
ghumbert <humbertgregoire@free.fr>
2012-06-03 15:30:01 +0200

    prodmondial

--- js/script.js
+++ js/script.js
@@ -21,10 +21,6 @@ function resizeContent(){
 }
 
 //#production_mondial
-var canevasProd = new Raphael(document.getElementById('canevasProd'),400,710);
-var biere = canevasProd.image("2_biere_03.png", 0, 0, 0, 0);
-biere.attrs = {cx: 0, cy: 605};
-biere.animate({width: 275, height:605},1000);
 
 
 
