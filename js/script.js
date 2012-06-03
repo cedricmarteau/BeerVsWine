1f43ba5f0644fdd1579f2ced0417cf5e11165c62
Tim <timothee.roussilhe@gmail.com>
2012-06-03 15:09:53 +0200

    Navigation au clavier et transition verticale.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+
+var windowWidth;			
+var windowHeight;
+var animating=false;
+
 $(document).ready(function(){
 	resizeContent();
+	setZindex();
 });
+
+
 
 // Fire on window resize.
 $(window).resize(function() {
@@ -11,11 +19,62 @@ $(window).resize(function() {
 function resizeContent(){
 	////console.log("resize");
 	// Resize content to fit window width.
-	var windowWidth = $(window).width();
+	windowWidth = $(window).width();
 				
-	var windowHeight = $(window).height();
+	windowHeight = $(window).height();
 	
 	var content = $("#content");
 	content.css({'width' : windowWidth, 'height' : windowHeight}).find(".pages").css({'width' : windowWidth, 'height' : windowHeight});
-	
 }
+
+function setZindex(){
+	var pages = $("#content").find('.pages');
+	pages.each(function(index){
+		if($(this).hasClass("current")){
+			$(this).css('z-index',"30");
+		}else{
+			$(this).css('z-index',pages.length-index);
+		}
+	});
+}
+function setPagePos(){
+	var pages = $("#content").find('.pages');
+	pages.each(function(index){
+		pages.css("top","0");
+	});
+}
+
+$(document).keydown(function(event){
+	var pages = $("#content").find('.pages');
+	var current = $("#content").find('.current');
+	var indexCurrent=current.index();
+	if(event.which=="40"){
+		if(pages.length>indexCurrent+1 && !animating){
+			animating=true;
+			pages.eq(indexCurrent+1).css('z-index',"29");
+			current.animate({"top":-windowHeight}, 500,function(){
+				animating=false;
+				current.removeClass("current");
+				pages.eq(indexCurrent+1).addClass("current");
+				setZindex();
+				setPagePos();
+			});
+			
+		}
+	}
+	if(event.which=="38"){
+		if(indexCurrent>0 && !animating){
+			animating=true;
+			pages.eq(indexCurrent-1).css('z-index',"29");
+			current.animate({"top":windowHeight}, 500,function(){
+				animating=false;
+				current.removeClass("current");
+				pages.eq(indexCurrent-1).addClass("current");
+				setZindex();
+				setPagePos();
+			});
+			
+		}
+	}
+	
+});
