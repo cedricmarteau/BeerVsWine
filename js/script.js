cb8f40656812cfff6890e36072ebcf3616ee9020
ghumbert <humbertgregoire@free.fr>
2012-06-05 00:49:59 +0200

    commit5

--- js/script.js
+++ js/script.js
@@ -49,19 +49,21 @@ $(document).ready(function(){
 	data_production();
 	import_export();
 	more_consume();
-<<<<<<< HEAD
 	best_consom();
-=======
-	//creerStats();
->>>>>>> 6090a0e02703547efce74639145cf3053d234d03
+	creerStatsParSexe();
+	creerStatsParAge();
+	creerStatsParRevenus();
 	nuage_tag();
+
 });
 
 
 
 //#consomation par..
 
-/*function creerStats(){
+
+
+function creerStatsParSexe(){
 
 	var largeurCanvas = 339;
 	var hauteurCanvas = 332;
@@ -80,30 +82,131 @@ $(document).ready(function(){
 
 	var hauteurFemmeVin = 238*51/100;
 	var hauteurFemmeBiere = 238*22/100;
-	var hauteurFemmeAutres = 238 - hauteurFemmeVin - hauteurFemmeAutres;
+	var hauteurFemmeAutres = 238 - hauteurFemmeVin - hauteurFemmeBiere;
 
 	var statsParSexe = new Raphael(document.getElementById('statsParSexe'),339,332);
 	
-	var rectangleHommeVin = statsParSexe.rect(0,0,115,hauteurHommeVin);
 
+	//Rectangle homme vin
+	var rectangleHommeVin = statsParSexe.rect(2,0,115,hauteurHommeVin);
 	rectangleHommeVin.attr({
 		'fill': "url(img/texture_vin.png)" ,
 		stroke: "none",
 		y: hauteurOffset + hauteurHomme
 	});
 
-	rectangleHommeVin.animate({y:hauteurOffset + hauteurHomme - hauteurHommeVin}, 1500,function(){
-		var rectangleHommeBiere = statsParSexe.rect(0,0,115,hauteurHommeVin);
+	rectangleHommeVin.animate({y:hauteurOffset + hauteurHomme - hauteurHommeVin}, 1000,function(){
+		var rectangleHommeBiere = statsParSexe.rect(2,0,115,hauteurHommeBiere);
+		rectangleHommeBiere.toBack();
+		rectangleHommeBiere.attr({
+			'fill': "url(img/texture_biere.png)" ,
+			stroke: "none",
+			y: hauteurOffset + hauteurHomme-hauteurHommeVin
+		});
+		rectangleHommeBiere.animate({y:hauteurOffset + hauteurHomme - hauteurHommeVin - hauteurHommeBiere}, 1000, function(){
+			var rectangleHommAutres = statsParSexe.rect(2,0,115,hauteurHommeAutres);
+			rectangleHommAutres.toBack();
+			rectangleHommAutres.attr({
+				'fill': "#d2d2d2" ,
+				stroke: "none",
+				y: hauteurOffset + hauteurHomme - hauteurHommeVin - hauteurHommeBiere
+			});
+			rectangleHommAutres.animate({y:hauteurOffset + hauteurHomme - hauteurHommeVin - hauteurHommeBiere - hauteurHommeAutres}, 1000);
+		});
+	});
 
+	var rectangleFemmeVin = statsParSexe.rect(116,0,115,hauteurFemmeVin);
+	rectangleFemmeVin.attr({
+		'fill': "url(img/texture_vin.png)" ,
+		stroke: "none",
+		y: hauteurOffset + hauteurHomme
+	});
+	rectangleFemmeVin.animate({y:hauteurOffset + hauteurHomme - hauteurFemmeVin}, 1000, function(){
+		var rectangleFemmeBiere = statsParSexe.rect(114,0,115,hauteurFemmeBiere);
+		rectangleFemmeBiere.toBack();
+		rectangleFemmeBiere.attr({
+			'fill': "url(img/texture_biere.png)" ,
+			stroke: "none",
+			y: hauteurOffset + hauteurHomme-hauteurFemmeVin
+		});
+		rectangleFemmeBiere.animate({y:hauteurOffset + hauteurHomme - hauteurFemmeVin - hauteurFemmeBiere},1000, function(){
+			var rectangleFemmeAutres = statsParSexe.rect(114,0,115,hauteurFemmeAutres);
+			rectangleFemmeAutres.toBack();
+				rectangleFemmeAutres.attr({
+				'fill': "#d2d2d2" ,
+				stroke: "none",
+				y: hauteurOffset + hauteurHomme - hauteurFemmeVin - hauteurFemmeBiere
+			});
+			rectangleFemmeAutres.animate({y:hauteurOffset + hauteurHomme - hauteurFemmeVin - hauteurFemmeBiere - hauteurFemmeAutres},1000);
+
+		});
 	});
 }
 
-/*
-	});*/
+function creerStatsParAge(){
+
+	var statsAge=[25/47*90,39/47*90,31/47*90,41/47*90,47/47*90,27/47*90];
+	var largeureMax = 90;
+
+	var vinJeune = $("#jeune .VinAge .pourcentageAge");
+	var biereJeune = $("#jeune .biereAge .pourcentageAge");
+
+	var vinAdulte = $("#adulte .VinAge .pourcentageAge");
+	var biereAdulte = $("#adulte .biereAge .pourcentageAge");
+
+	var vinVieux = $("#vieux .VinAge .pourcentageAge");
+	var biereVieux = $("#vieux .biereAge .pourcentageAge");
+
+	vinJeune.css({"width":0+"px"});
+	biereJeune.css({"width":0+"px"});
+	vinAdulte.css({"width":0+"px"});
+	biereAdulte.css({"width":0+"px"});
+	vinVieux.css({"width":0+"px"});
+	biereVieux.css({"width":0+"px"});
 
 
+	vinJeune.animate({'width':statsAge[0]+'px'}, 1000);
+	biereJeune.animate({'width':statsAge[1]+'px'}, 1000,function(){
+		vinAdulte.animate({'width':statsAge[2]+'px'}, 1000);
+		biereAdulte.animate({'width':statsAge[3]+'px'}, 1000,function(){
+			vinVieux.animate({'width':statsAge[4]+'px'}, 1000);
+			biereVieux.animate({'width':statsAge[5]+'px'}, 1000);
+		});
+	});
 
+}
 
+function creerStatsParRevenus(){
+
+	var hauteurMaxRevenus = 78;
+	var statsRevenus=[51/51*78,16/51*78,33/51*78,38/51*78,30/51*78,43/51*78,];
+
+	var jaugePauvresBiere = $("#pauvres .bierreRevenus .jaugeRevenus");
+	var jaugePauvresVin = $("#pauvres .vinRevenus .jaugeRevenus");
+	var jaugemoyensBiere = $("#moyens .bierreRevenus .jaugeRevenus");
+	var jaugemoyensVin= $("#moyens .vinRevenus .jaugeRevenus");
+	var jaugerichesBiere = $("#riches .bierreRevenus .jaugeRevenus");
+	var jaugerichesVin = $("#riches .vinRevenus .jaugeRevenus");
+
+	jaugePauvresBiere.animate({'height':statsRevenus[0]+'px'}, 3000);
+	jaugePauvresVin.animate({'height':statsRevenus[1]+'px'}, 3000);
+	jaugemoyensBiere.animate({'height':statsRevenus[2]+'px'}, 3000);
+	jaugemoyensVin.animate({'height':statsRevenus[3]+'px'}, 3000);
+	jaugerichesBiere.animate({'height':statsRevenus[4]+'px'}, 3000);
+	jaugerichesVin.animate({'height':statsRevenus[5]+'px'}, 3000);
+
+}
+
+/*,function(){
+						var rectangleFemmeAutres = statsParSexe.rect(0,0,115,hauteurFemmeAutres);
+						rectangleFemmeAutres.toBack();
+						rectangleFemmeAutres.attr({
+							'fill': "#d2d2d2" ,
+							stroke: "none",
+							y: hauteurOffset + hauteurHomme - hauteurFemmeVin - hauteurFemmeBiere
+						});
+						rectangleFemmeAutres.animate({y:hauteurOffset + hauteurHomme - hauteurFemmeVin - hauteurFemmeBiere - hauteurFemmeAutres}, 1000);
+					}*/
 
 
 
