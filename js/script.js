e87011c1201cd2c5b21b943c3df1b157111fe224
cedricmarteau <cedric.marteau@gmail.com>
2012-06-04 15:19:07 +0200

    plus gros consommateurs

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ function import_export(){
 //les plus consommés
 
 function more_consume(){
-	/*var data_beer_more_consum = new Array(45,42,46,42,36,40,40,36),
+	var data_beer_more_consum = new Array(45,42,46,42,36,40,40,36),
 	data_wine_more_consum = new Array(32,34,31,33,39,34,34,35),
 	data_liqueur_more_consum = new Array(18,19,18,22,21,22,21,23),
 	paper_width = 700,
@@ -165,19 +165,70 @@ function more_consume(){
 	}
 	beer_path.animate({path:line_beer_anim},1000);
 	wine_path.animate({path:line_wine_anim},1000);
-	//liqueur_path.animate({path:line_liqueur_anim},1000);*/
+	//liqueur_path.animate({path:line_liqueur_anim},1000);
 }
 
 //les plus gros consommateur
 function best_consom(){
+	var data_beer_best_consum = new Array(161.5,157,141.2,110.6,101.6,101.5,96.2,96.2,91.5,88.4),
+	data_wine_best_consum = new Array(62.2,60.13,55.85,52.70,48.16,46.67,43.77,42.27,39.87,34.66),
+	angle=360/10;
 	
-	
-	
-	
-	
-	
-	
-	
+	Raphael.fn.pieChart = function (cx, cy, r, values, labels, stroke) {
+			    var paper = this,
+			        rad = Math.PI / 180,
+			        chart = this.set();
+			    function sector(cx, cy, r, startAngle, endAngle, params) {
+			        var x1 = cx + r * Math.cos(-startAngle * rad),
+			            x2 = cx + r * Math.cos(-endAngle * rad),
+			            y1 = cy + r * Math.sin(-startAngle * rad),
+			            y2 = cy + r * Math.sin(-endAngle * rad);
+			        return paper.path(["M", cx, cy, "L", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"]).attr(params);
+			    }
+			    var angle = 0,
+			        total = 0,
+			        start = 0,
+			        process = function (j) {
+			            var value = values[j],
+			                angleplus = 360 * value / total,
+			                popangle = angle + (angleplus / 2),
+			                color = Raphael.hsb(start, .75, 1),
+			                ms = 500,
+			                delta = 30,
+			                bcolor = Raphael.hsb(start, 1, 1),
+			                p = sector(cx, cy, r, angle, angle + angleplus, {fill: "90-" + bcolor + "-" + color, stroke: stroke, "stroke-width": 3}),
+			                txt = paper.text(cx + (r + delta + 55) * Math.cos(-popangle * rad), cy + (r + delta + 25) * Math.sin(-popangle * rad), labels[j]).attr({fill: bcolor, stroke: "none", opacity: 0, "font-size": 20});
+			            p.mouseover(function () {
+			                p.stop().animate({transform: "s1.1 1.1 " + cx + " " + cy}, ms, "elastic");
+			                txt.stop().animate({opacity: 1}, ms, "elastic");
+			            }).mouseout(function () {
+			                p.stop().animate({transform: ""}, ms, "elastic");
+			                txt.stop().animate({opacity: 0}, ms);
+			            });
+			            angle += angleplus;
+			            chart.push(p);
+			            chart.push(txt);
+			            start += .1;
+			        };
+			    for (var i = 0, ii = values.length; i < ii; i++) {
+			        total += values[i];
+			    }
+			    for (i = 0; i < ii; i++) {
+			        process(i);
+			    }
+			    return chart;
+			};
+		
+			$(function () {
+			    var values = [],
+			        labels = [];
+			    $("tr").each(function () {
+			        values.push(parseInt($("td", this).text(), 10));
+			        labels.push($("th", this).text());
+			    });
+			    $("table").hide();
+			    Raphael("holder", 700, 700).pieChart(350, 350, 200, values, labels, "#fff");
+			});
 }
 
 //#sante
